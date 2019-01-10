77fcdee70b124ed218e49710ae5338f6c7bb8269
Ming <github@programmingbasics.org>
2019-01-10 18:37:58 -0500

    Starting off with a basic GWT Maven project.

---
+++ src/main/java/org/programmingbasics/loom/LoomPatternMakerEntry.java
@@ -0,0 +1,10 @@
+package org.programmingbasics.loom;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class LoomPatternMakerEntry implements EntryPoint
+{
+  public void onModuleLoad()
+  {
+  }
+}
