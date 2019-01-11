71447d90770ed5ffcb4d7e18e688008d528b3790
Ming <github@programmingbasics.org>
2019-01-11 18:41:15 -0500

    Basic mouse support for flipping a pattern stitch.

--- src/main/java/org/programmingbasics/loom/PatternCanvas.java
+++ src/main/java/org/programmingbasics/loom/PatternCanvas.java
@@ -3,6 +3,8 @@ package org.programmingbasics.loom;
 
 import org.programmingbasics.loom.PatternData.PatternRow;
 
+import elemental.events.Event;
+import elemental.events.MouseEvent;
 import elemental.html.CanvasElement;
 import elemental.html.CanvasRenderingContext2D;
 
@@ -16,23 +18,79 @@ public class PatternCanvas
   // Width and height of a stitch in pixels
   int stitchWidth = 22;
   int stitchHeight = 22;
+
+  int pillHeight = stitchHeight / 3;
+  int pillStraightWidth = stitchWidth - 2 * pillHeight;
+  int colorZoneX;
+  float colorBoxScale = 0.8f;
+
+  /** Whether the mouse button was depressed on the pattern portion of the canvas */
+  boolean isTrackingMouseOnPattern = false;
+  
+  /** Whether the mouse/touch is turning on pattern locations or turning them off */
+  boolean isMouseTurnOn = false;
   
   public PatternCanvas(CanvasElement canvas, PatternData data)
   {
     this.canvas = canvas;
     this.data = data;
     ctx = (CanvasRenderingContext2D)canvas.getContext("2d");
+
+    // Figure out sizing of stuff
+    colorZoneX = (data.width + 1) * stitchWidth;
+
+    // Hook UI events
+    canvas.addEventListener(Event.MOUSEDOWN, (e) -> {
+      MouseEvent evt = (MouseEvent)e;
+      evt.preventDefault();
+      evt.stopPropagation();
+      int row = findPatternRow(evt.getOffsetX(), evt.getOffsetY());
+      int col = findPatternCol(evt.getOffsetX(), evt.getOffsetY());
+      if (row >= 0 && row < data.height && col >= 0 && col < data.width)
+      {
+        isMouseTurnOn = !data.rows[row].data[col]; 
+        data.rows[row].data[col] = isMouseTurnOn;
+        isTrackingMouseOnPattern = true;
+        draw();
+      }
+    }, false);
+    canvas.addEventListener(Event.MOUSEMOVE, (e) -> {
+      MouseEvent evt = (MouseEvent)e;
+      evt.preventDefault();
+      evt.stopPropagation();
+      if (!isTrackingMouseOnPattern) return;
+      int row = findPatternRow(evt.getOffsetX(), evt.getOffsetY());
+      int col = findPatternCol(evt.getOffsetX(), evt.getOffsetY());
+      if (row >= 0 && row < data.height && col >= 0 && col < data.width)
+      {
+        data.rows[row].data[col] = isMouseTurnOn;
+        draw();
+      }
+    }, false);
+    canvas.addEventListener(Event.MOUSEUP, (e) -> {
+      MouseEvent evt = (MouseEvent)e;
+      evt.preventDefault();
+      evt.stopPropagation();
+      isTrackingMouseOnPattern = false;
+    }, false);
+  }
+  
+  private int findPatternRow(int mouseX, int mouseY)
+  {
+    return (int)(mouseY / stitchHeight);
+  }
+  
+  private int findPatternCol(int mouseX, int mouseY)
+  {
+    int row = findPatternRow(mouseX, mouseY);
+    if ((row % 2) != 0) mouseX -= stitchWidth / 2;
+    return (int)(mouseX / stitchWidth);
   }
   
   public void draw()
   {
     ctx.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
     
-    // Figure out sizing of stuff
-    int pillHeight = stitchHeight / 3;
-    int pillStraightWidth = stitchWidth - 2 * pillHeight;
-    int colorZoneX = (data.width + 1) * stitchWidth;
-    float colorBoxScale = 0.8f;
     
     // Start drawing
     ctx.save();
@@ -64,7 +122,7 @@ public class PatternCanvas
       
       // Draw a color box on the end
       ctx.beginPath();
-      ctx.rect(colorZoneX, centerY - stitchHeight / 2 * colorBoxScale, stitchWidth * colorBoxScale, stitchHeight * colorBoxScale);
+      ctx.rect(colorZoneX, (int)(centerY - stitchHeight / 2 * colorBoxScale), (int)(stitchWidth * colorBoxScale), (int)(stitchHeight * colorBoxScale));
       ctx.stroke();
     }
     ctx.restore();
